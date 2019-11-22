83ddd407901f96b46dda23b57a5768c83d674e17
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-11-23 00:06:33 +0300

    Optional type declaring

---
+++ src/main/scala/introduction/basic/OptionalTypeDeclaring.scala
@@ -0,0 +1,10 @@
+package introduction.basic
+
+/**
+ * @author Arthur Kupriyanov
+ */
+object OptionalTypeDeclaring extends App {
+  var a: String = "some value"  // preferred
+  var b = "some value"          // unnecessarily verbose
+
+}
