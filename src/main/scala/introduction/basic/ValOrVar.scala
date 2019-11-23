824423b8eecda8e31550fdebaf05c341bd8071fe
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-11-23 19:43:01 +0300

    added additional commentaries

--- src/main/scala/introduction/basic/ValOrVar.scala
+++ src/main/scala/introduction/basic/ValOrVar.scala
@@ -12,4 +12,20 @@ object ValOrVar extends App {
   // because a - it's immutable variable declared with "val" (like final in Java)
   b = "another value"
 
+  /*
+
+  But val is not equal to final in Java, at all.
+
+  If you use final to ref-param you can change his object's state (if fields not declared as final)
+
+  In Scala, val means your param is immutable (his state can't be changed)
+
+  In this case, for example, you can use ArrayList declared as val
+  var a:util.ArrayList[String] = new util.ArrayList[String]()
+
+  you can't invoke function add(...) because in this operation you change state of object
+
+
+   */
+
 }
